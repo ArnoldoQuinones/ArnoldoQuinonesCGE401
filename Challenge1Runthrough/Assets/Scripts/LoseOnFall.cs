c2f336bd19517ed210654553f7e8e0337a0c9944
ArnoldoQuinones <aquin5043@365.elmhurst.edu>
2025-09-08 23:13:06 -0500

    Added Challenge1Runthrough

---
+++ Challenge1Runthrough/Assets/Scripts/LoseOnFall.cs
@@ -0,0 +1,27 @@
+﻿/*
+* Arnoldo "Arnie" Quinones
+* Prototype 1
+* Code Description: Indicates Loss from Fall
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+//attach this script to the player
+public class LoseOnFall : MonoBehaviour
+{
+    //public Text textbox;
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -1)
+        {
+            //textbox.text = "You Lose!";
+            ScoreManager.gameOver = true;
+        }
+    }
+}
