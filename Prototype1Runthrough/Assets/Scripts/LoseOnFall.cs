926c189255f6775555c41df25beea8b24ee90df6
ArnoldoQuinones <aquin5043@365.elmhurst.edu>
2025-09-08 22:41:00 -0500

    Added Prototype1Runthrough

---
+++ Prototype1Runthrough/Assets/Scripts/LoseOnFall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+//attach this script to the player
+public class LoseOnFall : MonoBehaviour
+{
+    //public Text textbox;
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -1)
+        {
+            //textbox.text = "You Lose!";
+            ScoreManager.gameOver = true;
+        }
+    }
+}
