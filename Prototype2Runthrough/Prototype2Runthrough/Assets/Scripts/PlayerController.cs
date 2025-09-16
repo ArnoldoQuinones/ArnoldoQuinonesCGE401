deded6dfcd1ea591af6a5d77e6491e59bfd1c54d
ArnoldoQuinones <aquin5043@365.elmhurst.edu>
2025-09-16 15:33:16 -0500

    Prototype2Finished

---
+++ Prototype2Runthrough/Prototype2Runthrough/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horiztonalInput;
+    public float speed = 10.0f;
+    private float xRange = 14;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Get horizontal input
+        horiztonalInput = Input.GetAxis("Horizontal");
+        
+        //transform horizontally with our input
+        transform.Translate(Vector3.right * horiztonalInput * Time.deltaTime * speed);
+        
+        //Keep player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
+    }
+}
