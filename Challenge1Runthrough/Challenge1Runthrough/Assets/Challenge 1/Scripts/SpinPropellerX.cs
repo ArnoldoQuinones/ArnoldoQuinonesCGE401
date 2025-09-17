f6425bc59b83370df128ff32c04fad4969a41613
ArnoldoQuinones <aquin5043@365.elmhurst.edu>
2025-09-17 01:46:20 -0500

    Challenge 1 Complete

---
+++ Challenge1Runthrough/Challenge1Runthrough/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,20 @@
+﻿/*
+* Arnoldo "Arnie" Quinones
+* Prototype 1
+* Code Description: Command for Propeller Spin Speed
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float rotationSpeed = 1000f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Challenge1Runthrough/Challenge1Runthrough/Assets/Challenge 1/Scripts/TriggerZoneAddScoreOnceX.cs
@@ -0,0 +1,24 @@
+﻿/*
+* Arnoldo "Arnie" Quinones
+* Prototype 1
+* Code Description: Adding Trigger Zone Points
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach this to a trigger zone
+public class TriggerZoneAddScoreOnceX : MonoBehaviour
+{
+    private bool triggered = false;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            ScoreManagerX.score++;
+        }
+    }
+}
