f9beb440ceedd25a6a19e38f82fa6359572529e4
ArnoldoQuinones <aquin5043@365.elmhurst.edu>
2025-09-30 10:56:09 -0500

    Challenge 3 Complete

---
+++ Challenge3Runthrough/Assets/Challenge 3/Scripts/ScoreManagerX.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManagerX : MonoBehaviour
+{
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+    
+    public Text textbox;
+    
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //If the game is not over, display score
+        if (!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+        
+        //win condition: 30 or more points
+        if (score >= 30)
+        {
+            won = true;
+            gameOver = true;
+        }
+
+        if (gameOver)
+        {
+            if (won)
+            {
+                textbox.text = "You Win!\nPress R to Try again!";
+            }
+            else
+            {
+                textbox.text = "You Lose!\nPress R to Try again!";
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
